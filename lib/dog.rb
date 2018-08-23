26f8084aaa05ef593924570bb5bce3cf91d32cc3
Heshie Brody <4hbrody@gmail.com>
2018-08-23 03:31:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,10 @@ class Dog
     @name = name
     @@all << self
   end
-  def self.all
-    @all.each{|dog| puts dog.name}
-  end
   def self.clear_all
     @@all.clear
   end
-
+  def self.all
+    @all.each{|dog| puts dog.name}
+  end
 end
